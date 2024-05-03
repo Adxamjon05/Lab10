179fd2d4409460b236ed5bd04c923737dc6b78b7
Adxamjon05 <161422394+Adxamjon05@users.noreply.github.com>
2024-05-03 18:22:01 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,56 @@
 #include <iostream>
-#include <math.h>
+
+
 using namespace std;
 
+template <typename T>
+T largest(T a, T b, T c){
+    if (a>b and a>c){
+        return a;
+    } else if (b>c){
+        return b;
+    } else{
+        return c;
+    }
+}
+template<typename T>
+T swap1(T& a, T& b){
+    a=a+b;
+    b=a-b;
+    a=a-b;
+}
+
+
+
+double avg4(double n4){
+    double num4;
+    double sum4=0;
+    for (int i=0; i<n4; i++){
+        cin>>num4;
+        sum4+=num4;
+    }
+    return sum4/n4;
+}
 
 
 
 int main() {
+    int n3;
+    cout<<"n=";
+    cin>>n3;
+    int sum3, num3;
+    sum3=0;
+    for (int i=0; i<n3; i++){
+        cin>>num3;
+        sum3+=num3;
+    }
+    cout<<"sum="<<sum3<<endl;
 
 
 
-
+    double n4;
+    cin>>n4;
+    cout<<avg4(n4);
 
 
 
